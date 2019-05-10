9d4c6fa6410473b1273a2730d5b45188cfa67f5e
Ajay Marathe <ajay.marathe@xebecindia.com>
2019-05-10 13:08:13 +0530

    like functionlity done

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Http\Request;
 Route::apiResource('/question','QuestionController');
 Route::apiResource('/category','CategoryController');
 Route::apiResource('/question/{question}/reply','ReplyController');
+Route::post('/like/{reply}','LikeController@liktIt');
+Route::delete('/like/{reply}','LikeController@unLikeIt');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
